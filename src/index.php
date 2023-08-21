c4230bdfe0ceb0cdc9235f0f2076caf2235eac5a
bwuermli <benjamin.wuermli@bertschi.com>
2023-08-21 11:50:03 +0200

    Initial commit

---
+++ src/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Snake</title>
+    <link rel="stylesheet" href="snake_style.css">
+    <!-- defer stands for loading after index.php
+     we need type="module" because we import and export from different .js files at the same time-->
+    <script src="game.js" defer type="module"></script>
+</head>
+
+<body>
+<div id="game-board"></div>
+</body>
+
+</html>
